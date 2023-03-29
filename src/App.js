edec34c9adf785ca7f128af8a0a04d2ce82bb8e3
RaisulislamRimon <51745662+RaisulislamRimon@users.noreply.github.com>
2023-03-30 04:15:01 +0600

    1 file modified, 1 added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './style.css';
+import Nav from './components/Home/Nav';
 
 export default function App() {
   return (
     <div>
+      <Nav />
       <h1>Hello World!</h1>
     </div>
   );
